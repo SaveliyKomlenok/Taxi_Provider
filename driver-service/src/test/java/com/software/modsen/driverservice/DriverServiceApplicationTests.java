5b0ae43b5d9ada8a3932cb8b106284f786ef6ba6
Saveliy Komlenok <skomlenok363@gmail.com>
2025-02-12 17:51:23 +0300

    fix: tests

--- driver-service/src/test/java/com/software/modsen/driverservice/DriverServiceApplicationTests.java
+++ driver-service/src/test/java/com/software/modsen/driverservice/DriverServiceApplicationTests.java
@@ -3,7 +3,6 @@ package com.software.modsen.driverservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class DriverServiceApplicationTests {
 
     @Test
